4e67edc828c3dc965b709650abf5128e777ecb9a
Ashwini Seshadri <ashwinikseshadri13@gmail.com>
2022-08-29 13:46:38 +0200

    user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true, 
+    length: { minimum: 10, allow_blank: true }
+  validates :email, presence: true,
+   format: { with: /\S+@\S+/ },
+    uniqueness: { case_sensitive: false }
+
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
 end
