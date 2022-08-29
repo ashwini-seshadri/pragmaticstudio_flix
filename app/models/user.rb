f631197065e88aaf22535e0ec4b066358d72ecf1
Ashwini Seshadri <ashwinikseshadri13@gmail.com>
2022-08-29 15:47:20 +0200

    user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   validates :email, presence: true,
    format: { with: /\S+@\S+/ },
     uniqueness: { case_sensitive: false }
+  validates :username, presence: true,
+    format: { with: /\A[A-Z0-9]+\z/i }, uniqueness: { case_sensitive: false }
 
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
